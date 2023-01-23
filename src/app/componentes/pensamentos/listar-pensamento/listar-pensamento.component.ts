4be2a430f818493722d5abf7ac6be80ff4f3a342
Leonardo Florêncio <leonardo_florenciop@outlook.com>
2023-01-23 06:38:59 -0300

    Criação API JSON e doc service para as regras CRUD

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -7,23 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
-  listaPensamentos = [
-    {
-      conteudo: 'Passo informações para um componente filho',
-      autoria: 'Componente pai',
-      modelo: 'modelo3'
-    },
-    {
-      conteudo: 'Minha propriedade é decorada com o @Input()',
-      autoria: 'Componente filho',
-      modelo: 'modelo2'
-    },
-    {
-      conteudo: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam porta ut libero vitae ultricies. Mauris tristique orci tortor, ut congue risus molestie eget. Donec id consequat mi. Suspendisse potenti. Fusce ullamcorper vehicula elit, sed mattis nunc mattis vitae. Proin ac nisi felis. Morbi at semper ipsum. Ut quis nibh vel nunc congue vestibulum. Suspendisse at volutpat nisl. Aliquam at tellus luctus risus fermentum ultricies vitae sed enim. Nullam in ligula tincidunt, euismod est eu, interdum tellus. Suspendisse blandit velit eu elit facilisis, non elementum felis viverra.',
-      autoria: 'Lipsum',
-      modelo: 'modelo1'
-    },
-  ];
+  listaPensamentos: Pensamento[] = [];
 
   constructor() { };
 
